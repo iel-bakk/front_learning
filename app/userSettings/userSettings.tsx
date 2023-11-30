f898ab9b4e1cfc2bf36029be6dcf8514e00a80ff
iel-bakk <iel-bakk@student.1337.ma>
2023-11-30 19:29:40 +0100

    update

--- app/userSettings/userSettings.tsx
+++ app/userSettings/userSettings.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+'use client';
 import Card from "./Card";
+import React, { useEffect, useState } from 'react';
 
+type userSettingsData = {
+    invitations : string[];
+    friends     : string[];
+    bandUsers   : string[];
+};
+ 
 function UserSettings() {
+    const [data, setData] = useState<userSettingsData>();
+    
+    const fetchInfo = () => { 
+        return fetch("http://localhost:5000/Chat/userSettings", {
+            method : "GET",
+            credentials : 'include'
+        }) 
+                .then((res) => res.json()) 
+                .then((d) => 
+                {
+                    setData(d)
+                    console.log(d);
+                }).catch((error) => {
+                    console.error('Error:', error);
+                })
+        }
+        
+        useEffect(() => {
+            fetchInfo();
+        }, [])
     return (
             <div className="h-full w-full flex flex-row items-center justify-around">
                 <Card/>
