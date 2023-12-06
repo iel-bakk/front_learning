7d59160eb962bc07f50d0a02b65dc2aef4fe0755
iel-bakk <iel-bakk@student.1337.ma>
2023-12-07 00:55:51 +0100

    fixed style

--- app/channel/channels.client.tsx
+++ app/channel/channels.client.tsx
@@ -14,6 +14,7 @@ type channelMessages = {
 function ChannelChat() {
  const [channelNames, setData] = useState<channelNames>();
  const [messages, setMessages] = useState<channelMessages[]>([]);
+ const [ChoosenChannel, SetChoosenChannel] = useState<string>("Channel Name");
 
  async function handleClick(name: string) {
      console.log(`http://localhost:4000/Chat/channel`);
@@ -29,6 +30,7 @@ function ChannelChat() {
      const data = await response.json();
      setMessages(data);
      console.log("message : ====> ", data);
+     SetChoosenChannel(name);
  }
 
  const fetchInfo = async () => { 
@@ -58,28 +60,38 @@ function ChannelChat() {
      </div>)
 
  return (
-     <div className="h-[80%] w-full flex flex-row items-center justify-around">
-         <div className=' w-[30%] h-full flex flex-col items-center rounded-lg border border-[#E58E27]'>
-             <h3 className='p-4'>conversations</h3>
-             <div className='w-full h-full text-white flex flex-col items-center'>
-               {channelNames && channelNames?.channelNames.map((channel, index) => {
-               return (
-                <div className='bg-grey-100' key={index} style={{cursor: 'pointer'}} onClick={() => handleClick(channel)}>{channel}</div>
-                //   <div className='bg-grey-100' key={index}><span onClick={() => handleClick(channel)} style={{pointerEvents: 'auto'}}>{channel}</span></div>
-               );})}
-             </div>
-         </div>
-         <div className=' w-[60%] h-full flex flex-col items-center rounded-lg border border-[#E58E27]'>
-             <h3 className='p-4'>CHANNEL MESSAGES</h3>
-             {messages && messages.map((message, index) => (
-                <div key={index} className="flex flex-row justify-around">
-                <p>{message.sender}</p>
-                <p>{message.content}</p>
+    <div className="relative h-[80%] w-full flex flex-row md:flex-col items-center justify-around">
+    <div className=' w-[30%] h-full flex flex-col items-center rounded-lg border border-[#E58E27]'>
+   <h3 className='p-4'>conversations</h3>
+   <div className='w-full h-full text-white flex flex-col items-center overflow-y-auto scrollbar-hide'>
+       {channelNames && channelNames?.channelNames.map((channel, index) => {
+           return (
+             <div className='w-full h-full text-center bg-gray-500 p-4 border border-[#E58E27]' key={index} style={{cursor: 'pointer'}} onClick={() => handleClick(channel)}>{channel}</div>
+           );
+       })}
+   </div>
+</div>
+
+    <div className='overflow-hidden w-[60%]  h-full flex flex-col items-center rounded-lg border border-[#E58E27] relative'>
+        <div className='w-full text-center border border-[#E58E27]'><h3 className='p-4'>{ChoosenChannel}</h3></div>
+        <div>
+            {messages && messages.map((message, index) => (
+                <div key={index} className="w-full h-full flex flex-row justify-around p-4 overflow-y-auto scrollbar-hide">
+                  <p>{message.sender}</p>
+                  <p>{message.content}</p>
                 </div>
-             ))}
-         </div>
-     </div>
- );
+            ))}
+        </div>
+        <div className='absolute bottom-0 w-full flex-end rounded-lg border  border-black flex'>
+          <input className="w-full text-black rounded-lg border border-black"/>
+          <button className="w-1/10 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Send
+          </button>
+        </div>
+    </div>
+   </div>
+   
+  );
 }
 
 export default ChannelChat;
