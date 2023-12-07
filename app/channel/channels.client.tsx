22e1aaae2559da9cbe45244fa011b3d3bf3645e9
iel-bakk <iel-bakk@student.1337.ma>
2023-12-07 23:22:23 +0100

    updated

--- app/channel/channels.client.tsx
+++ app/channel/channels.client.tsx
@@ -4,11 +4,12 @@ import { WebsocketContext } from '../Contexts/socket';
 
 type channelNames = {
  channelNames:string[],
+ username : string
 };
 
 type channelMessages = {
  sender : string,
- content : string[],
+ content : string,
  channelName : string
 }
 
@@ -18,6 +19,7 @@ function ChannelChat() {
  const [channelMessages, setChannelMessages] = useState<channelMessages>();
  const [messages, setMessages] = useState<channelMessages[]>([]);
  const [ChoosenChannel, SetChoosenChannel] = useState<string>("Channel Name");
+ const [inputValue, setInputValue] = useState('');
   socket.on("channelMessage", res=>{
     setChannelMessages(res.data)
     console.log(res);
@@ -61,7 +63,7 @@ function ChannelChat() {
 
  if (!channelNames?.channelNames)
      return (
-     <div className='flex justify-center'>
+     <div className='flex justify-center position-center'>
          <h1 className='text-orange-500 truncate'>CHAT IS LOADING ... THANKS FOR WAITING ...</h1>
      </div>)
 
@@ -80,19 +82,41 @@ function ChannelChat() {
 
     <div className='overflow-hidden w-[60%]  h-full flex flex-col items-center rounded-lg border border-[#E58E27] relative'>
         <div className='w-full text-center border border-[#E58E27]'><h3 className='p-4'>{ChoosenChannel}</h3></div>
-        <div className='w-full h-full flex flex-col'>
+        <div className='w-full h-full flex flex-col overflow-y-auto scrollbar-hide'>
             {messages && messages.map((message, index) => (
-                <div key={index} className="flex flex-row w-[50%] rounded-lg justify-around bg-[#323232] p-2 m-4  overflow-y-auto scrollbar-hide">
-                  <p>{message.sender}</p>
+                <div key={index} className="flex flex-row w-[50%] rounded-lg justify-around bg-[#323232] p-2 m-4">
+                  <p>{message.sender} :</p>
                   <p>{message.content}</p>
                 </div>
             ))}
         </div>
         <div className='absolute bottom-0 w-full flex-end rounded-lg border  border-black flex'>
-          <input className="w-full text-white rounded-lg border border-black bg-[#323232] p-4"/>
-          <button className="w-1/10 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
-            Send
-          </button>
+        <input 
+         className="w-full text-white rounded-lg border border-black bg-[#323232] p-4"
+         value={inputValue}
+         onChange={(e) => setInputValue(e.target.value)}
+       />
+       <button 
+ className="w-1/10 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
+ onClick={() => {
+   const newMessage = {
+     sender: channelNames.username, // replace 'Your Name' with the actual sender's name
+     content: inputValue,
+     channelName: ChoosenChannel,
+   };
+   setMessages((prevMessages) => [...prevMessages, newMessage]);
+   socket.emit('channelMessage', newMessage);
+ }}
+>
+ Send
+</button>
+
+       {/* <button 
+         className="w-1/10 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
+         onClick={() => socket.emit('channelMessage', { content: inputValue, channelName : ChoosenChannel })}
+       >
+         Send
+       </button> */}
         </div>
     </div>
    </div>
